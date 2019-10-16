ca5f81376a638ab1f41e03c3c864e67567de4e83
Pete <mistermcgreen@gmail.com>
2019-10-16 13:53:46 -0500

    fixed setup script

--- setup.sh
+++ setup.sh
@@ -1,1 +1,3 @@
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+#!/bin/bash
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \ 
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim

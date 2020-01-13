f70a2cf107efa389a2f04d37a36142678d6769ad
Pete Canfield <mistermcgreen@gmail.com>
2020-01-13 17:45:51 -0600

    Made changes to setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \ 
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
